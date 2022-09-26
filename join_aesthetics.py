ebe2b5eccc82aed47df9addc887bb6f7fcfe0af4
Marianna <43296932+marianna13@users.noreply.github.com>
2022-09-26 14:12:48 +0300

    Add join_aesthetics.py

---
+++ join_aesthetics.py
@@ -0,0 +1,21 @@
+from pyspark.sql import SparkSession
+import os
+from pyspark.sql import DataFrame
+
+
+spark = SparkSession.builder.config("spark.driver.memory", "500G").config("spark.sql.files.ignoreCorruptFiles", "true").config("spark.local.dir", "/scratch/marianna").master("local[96]").appName('spark-stats').getOrCreate()
+
+
+file_path_list = os.listdir('/scratch/marianna/FULL')
+file_path_list = [f'/scratch/marianna/FULL/{f}' for f in file_path_list]
+
+ast_path_list = os.listdir('/fsx/marianna/translate_dataset/aesthetics')
+ast_path_list = [f'/fsx/marianna/translate_dataset/aesthetics/{f}' for f in ast_path_list]
+
+df = spark.read.parquet(*file_path_list)
+ast_df = spark.read.parquet(*ast_path_list)
+ast_df = ast_df.select("prediction", "hash")
+
+extra_df = df.join(ast_df, ["hash"], 'left') 
+
+extra_df.write.parquet("/scratch/marianna/JOINED")
